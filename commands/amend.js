8b381594874e06124f00d4ee49d9578149f34008
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2021-07-24 14:31:57 +0100

    Update amend.js

--- commands/amend.js
+++ commands/amend.js
@@ -4,7 +4,6 @@ exports.run = async (message, args) => {
   var scoreboard = base.scoreboard;
   var dictionary = base.team_dictionary;
   var admin_channel = base.admin_channel;
-  var team_result_embeds = base.answer_embeds;
   try {
     var round_number = args[0];
     var team = args[1];
@@ -20,16 +19,32 @@ exports.run = async (message, args) => {
     }
     if(operator === "+"){
       if(typeof amendment === 'number'){
-        message.reply("Congrats!  This score addition will work, if you code it here.")
-        console.log(team_result_embeds[round_number][team])
+        //var relevent_round = scoreboard[team][round_number];
+        //var score = parseFloat(relevant_round.split('/')[0]);
+        //var scorePoss = parseFloat(relevant_round.split('/')[1]);
+        //var new_score = score + amendment;
+        //var new_score_push = new_score + "/" + scorePoss;
+        //await base.scoreboardParse(team, round_number, new_score_push);
+        //let teamChannel = message.guild.channels.cache.find(channel => channel.name === team);
+        message.reply("Congrats!  This score addition will work, if you code it here.");
+        console.log(scoreboard[team][round_number]);
+        if(teamChannel){
+          teamChannel.send(message.author + " has just added " + amendment + " points to your score for Round " + round_number + ".  Your score for Round " + round_number + " is now " new_score_push);
+        }
       } else {
         message.reply("I need to know the amount to add to the score - the amount must be a number.  " + amendment + " is not a number.");
         return;
       }
     } else if(operator === "-"){
       if(typeof amendment === 'number'){
-        message.reply("Congrats!  This score subtraction will work, if you code it here.")
-        console.log(team_result_embeds[round_number][team])
+        //var relevent_round = scoreboard[team][round_number];
+        //var score = parseFloat(relevant_round.split('/')[0]);
+        //var scorePoss = parseFloat(relevant_round.split('/')[1]);
+        //var new_score = score - amendment;
+        //var new_score_push = new_score + "/" + scorePoss;
+        //await base.scoreboardParse(team, round_number, new_score_push);
+        //let teamChannel = message.guild.channels.cache.find(channel => channel.name === team);
+        //teamChannel.send(message.author + " has just deducted " + amendment + " points from your score for Round " + round_number + ".  Your score for Round " + round_number + " is now " new_score_push);
       } else {
         message.reply("I need to know the amount to subtract from the score - the amount must be a number.  " + amendment + " is not a number.");
         return;
