713cb22f148585deac5524131df895e11addcd8c
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2021-07-25 00:52:06 +0100

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -8,10 +8,13 @@ exports.run = async (message, args) => {
   try {
     // ------------------------ EDIT BELOW THIS LINE AS MUCH AS YOU LIKE -----------------------------------------
     request.get("https://script.google.com/macros/s/AKfycby2Wqb6CaV-EUiWVSTUZ6S0ZDH8yiFs6mL9KnM_gC1jWX6mv3LQYKuqCxKP-rdbUIessA/exec", async (error, response, body) => {
-        console.log("Triggering Forms...");
+      console.log("Triggering Forms...");
+      console.log(error)
+      console.log(response)
+      console.log(body)
         if (!error && response.statusCode == 200) {
           var success = body;
-          console.log(success);
+          console.log("Triggered Forms!");
         }
       });   
     // ------------------------ EDIT ABOVE THIS LINE AS MUCH AS YOU LIKE -----------------------------------------
