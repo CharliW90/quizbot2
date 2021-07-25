3c303e6e280e24695aba5088c0be41c189bfe2c7
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2021-07-25 01:33:23 +0100

    Create collect.js

---
+++ commands/collect.js
@@ -0,0 +1,17 @@
+exports.run = async (message, args) => {
+const puppeteer = require('puppeteer');
+const url = process.env.googleScript;
+  try {
+    let parameter  = args[0];
+    if (parameter === "answers"){
+      console.log("Triggering Google Script at: " + url);
+      (async () => {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto(url);
+        await browser.close();
+      })();
+  } catch (e) {
+    throw e;
+  }
+}
