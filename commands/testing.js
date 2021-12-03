89f258c076516ae87653dc86f2a92271013b0147
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2021-12-03 18:08:51 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -1,22 +1,28 @@
 exports.run = async (message, args) => {
-  var base = require('/app/bot.js');
-  var Discord = base.Discord;
-  var teamCount = base.teamCount;
-  var admin_channel = base.admin_channel;
-  var teamsObject = require('/app/objects/teams.js');
-  const puppeteer = require('puppeteer');
-  const url = process.env.googleScript;
-  try {
-    console.log(url);
-    // ------------------------ EDIT BELOW THIS LINE AS MUCH AS YOU LIKE -----------------------------------------
-    (async () => {
-      const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-      const page = await browser.newPage();
-      await page.goto(url);
-      await browser.close();
-    })();
-    // ------------------------ EDIT ABOVE THIS LINE AS MUCH AS YOU LIKE -----------------------------------------
-  } catch (e) {
-    throw e;
-  }
+    var base = require('/app/bot.js');
+    var Discord = base.Discord;
+    var teamCount = base.teamCount;
+    var admin_channel = base.admin_channel;
+    try {
+        await message.guild.members.fetch()
+          .catch(console.error);
+        if (message.guild.channels.cache.find(CategoryChannel => CategoryChannel.name == "Quiz Teams") == undefined) {
+            message.reply('Cannot find the Quiz Teams Category');
+        } else {
+            const quizTeamChannels = message.guild.channels.cache.find(CategoryChannel => CategoryChannel.name == "Quiz Teams").children;
+            quizTeamChannels.forEach(channel => {
+                if (channel.type == 'text') {
+                  //we only want to get the role from the first channel we encounter, because by the time we get to the second it has already been deleted
+                  if (message.guild.roles.cache.find(role => role.id === channel.permissionOverwrites.array()[1].id)) {
+                    console.log(channel.permissionOverwrites);
+                    console.log(channel.permissionOverwrites.array());
+                  }
+                }
+            });
+        } else {
+            message.reply('there is nothing to reset!');
+        }
+    } catch (e) {
+        throw e;
+    }
 }
