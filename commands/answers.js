caba92d93a597c06f6317720e7cc152e336fadb5
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2022-07-11 22:03:39 +0100

    Update answers.js

--- commands/answers.js
+++ commands/answers.js
@@ -115,7 +115,7 @@ exports.run = async (message, args) => {
     } else {
       var clean_message = message.cleanContent;
       var input = await clean_message.substring(clean_message.indexOf('"'));
-      if (input == "") {
+      if (input === "") {
         console.log("No input substring was found in the message - returning error message...");
         message.reply("You haven't provided a table of scores (no attachment, and no data provided after the round number in the command).  Did you mean to use the command ++results " + round_num + " instead??");
         return;
