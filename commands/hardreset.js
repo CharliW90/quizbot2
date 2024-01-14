ee56c0325ca9598780fbb2788ca8f0298d84562f
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-14 15:29:55 +0000

    Update hardreset.js
    remove verbose console.log() and amend code to use .rawPosition of a role, not the id

--- commands/hardreset.js
+++ commands/hardreset.js
@@ -6,13 +6,10 @@ exports.run = async (message, args) => {
     console.log(`Hard Reset requested by ${message.author.name}`);
     try {
         const allRoles = message.guild.roles.fetch();
-        const teamPos = message.guild.roles.cache.find(role => role.name === 'Team Captain') - 1;
-        console.log(`the teamPos is ${teamPos}`);
-        console.log("Roles:");
-        console.log(message.guild.roles.cache)
+        const teamPos = message.guild.roles.cache.find(role => role.name === 'Team Captain').rawPosition;
         message.guild.roles.cache.forEach((role) => {
-          if(role <= teamPos){
-            console.log(role)
+          if(role.rawPosition < teamPos && role.rawPosition > 0){
+            console.log(role.name)
             message.reply(`Testing - this command would have deleted ${role}`)
           }
         })
