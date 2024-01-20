7523e620b9ca3572b6290aa935529c7ac8db4ba0
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 17:03:26 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -6,6 +6,7 @@ exports.run = async (message, args) => {
     var quizmasters = await message.guild.roles.cache.find(role => role.name === 'Quizmaster').members;
     try {
         console.log(quizmasters)
+        await quizmasters.forEach((member) => {console.log(member)})
         //quizmasters.forEach((element) => {console.log(element)})
         //message.reply(`A: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters1}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
         //message.reply(`B: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters2}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
