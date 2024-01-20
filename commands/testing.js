a13c562465a7b645f74849a8a1095cf89a83573b
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 18:00:13 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -8,22 +8,17 @@ exports.run = async (message, args) => {
     try {
         var quizmasterMembers = [];
         await quizmasters.forEach((member) => {
-            console.log("member found");
-            console.log(member.id);
-            console.log(member._roles);
-            console.log(member.keys);
-            console.log(member.name);
-            console.log(member);
             quizmasterMembers.push(member);
         });
-        message.reply(`TEST: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasterMembers.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
+        console.log(quizmasterMembers)
+        await message.channel.send(`TEST: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasterMembers.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
+        await message.delete();
         await quizmasters.forEach((member) => {
-            member.forEach((unknownThing) => {
-                console.log(unknownThing);
-            });
+            for(const key of member){
+                console.log(key);
+            }
         });
     } catch (e) {
         throw e;
     }
-    message.delete()
 }
