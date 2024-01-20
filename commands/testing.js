62bc6adaf9f92a3767f40feeebf6e0c02a88d492
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 16:56:40 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -3,15 +3,9 @@ exports.run = async (message, args) => {
     var Discord = base.Discord;
     var teamCount = base.teamCount;
     var admin_channel = base.admin_channel;
-    var quizmasters1 = message.guild.roles.cache.find(role => role.name === 'Quizmaster').members.map(m=>m.user.tag).join(',');
-    var quizmasters2 = message.guild.roles.cache.find(role => role.name === 'Quizmaster').members;
+    var quizmasters = message.guild.roles.cache.find(role => role.name === 'Quizmaster');
     try {
-        console.log("1");
-        console.log(quizmasters1[0]);
-        console.log([...quizmasters1]);
-        console.log("2");
-        console.log(quizmasters2[0]);
-        console.log([...quizmasters2]);
+        quizmasters.forEach((element) => {console.log(element)})
         //message.reply(`A: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters1}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
         //message.reply(`B: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters2}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
         //message.reply(`C: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters1.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
