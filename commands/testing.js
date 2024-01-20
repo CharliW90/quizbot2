7ff8f878658d8fe4d1246130d0ff5e2c56ae31e1
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 18:03:22 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -6,18 +6,18 @@ exports.run = async (message, args) => {
     var members = await message.guild.members.fetch();
     var quizmasters = await message.guild.roles.cache.find(role => role.name === 'Quizmaster').members;
     try {
+        //  put test code below this line ---------------
+        
         var quizmasterMembers = [];
         await quizmasters.forEach((member) => {
             quizmasterMembers.push(member);
         });
         console.log(quizmasterMembers)
         await message.channel.send(`TEST: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasterMembers.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
+        
+        
+        //  put test code above this line ---------------
         await message.delete();
-        await quizmasters.forEach((member) => {
-            for(const key of member){
-                console.log(key);
-            }
-        });
     } catch (e) {
         throw e;
     }
