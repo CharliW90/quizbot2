a72c81627d68c36b82879ebde089403a2838eb05
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 17:24:13 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -11,6 +11,10 @@ exports.run = async (message, args) => {
             console.log("member found");
             console.log(member.id);
             console.log(member._roles);
+            console.log(member.keys());
+            console.log(member.keys);
+            console.log(member.name);
+            console.log(member);
         })
         //message.reply(`D: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters2.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
     } catch (e) {
