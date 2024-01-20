a56a24e45cdfeadbb65df2f1ad5dfbbc1a329df9
Charli <72148151+CharliW90@users.noreply.github.com>
2024-01-20 16:53:22 +0000

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -4,14 +4,18 @@ exports.run = async (message, args) => {
     var teamCount = base.teamCount;
     var admin_channel = base.admin_channel;
     var quizmasters1 = message.guild.roles.cache.find(role => role.name === 'Quizmaster').members.map(m=>m.user.tag).join(',');
-    console.log(quizmasters1);
     var quizmasters2 = message.guild.roles.cache.find(role => role.name === 'Quizmaster').members;
-    console.log(quizmasters2)
     try {
+        console.log("1");
+        console.log(quizmasters1[0]);
+        console.log([...quizmasters1]);
+        console.log("2");
+        console.log(quizmasters2[0]);
+        console.log([...quizmasters2]);
         //message.reply(`A: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters1}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
         //message.reply(`B: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${...quizmasters2}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
-        message.reply(`C: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters1.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
-        message.reply(`D: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters2.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
+        //message.reply(`C: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters1.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
+        //message.reply(`D: Hi there, and welcome to the Virtual Quiz!  :grin:  As the Team Captain, you are able to use my 'add', 'remove', and 'promote' commands and you also have access to the 'ask-the-quizmasters' text channel to speak to ${quizmasters2.join(',')}.  If you need any help with my commands just use ++help in your text channel.  Good luck, have fun!  :heart:`);
     } catch (e) {
         throw e;
     }
